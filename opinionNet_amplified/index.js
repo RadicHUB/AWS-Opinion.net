c6b64fcb2eb9f5351af3e74cd48fc809ea3a340e
RadicHUB <milesems2001@gmail.com>
2022-11-03 14:48:19 -0400

    reload main to branch

--- opinionNet_amplified/index.js
+++ opinionNet_amplified/index.js
@@ -2,38 +2,13 @@
  * @format
  */
 
-import '@azure/core-asynciterator-polyfill';
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-import { Amplify } from 'aws-amplify';
-import awsconfig from './src/aws-exports';
-
-// check if env is localhost or not
-// if you're not developing on localhost, you will need to detect this is another way—the docs linked above give some examples. 
-const isLocalhost = !!(window.location.hostname === "localhost");
-
-// split redirect signin and signout strings into correct URIs
-const [
-productionRedirectSignIn,
-localRedirectSignIn ] = awsconfig.oauth.redirectSignIn.split(",");
-const [
-productionRedirectSignOut,
-localRedirectSignOut ] = awsconfig.oauth.redirectSignOut.split(",");
-
-// use correct URI in the right env
-const updatedAwsConfig = {
-...awsconfig,
-oauth: {
-...awsconfig.oauth,
-redirectSignIn: isLocalhost 
-    ? localRedirectSignIn 
-    : productionRedirectSignIn,
-redirectSignOut: isLocalhost 
-    ? localRedirectSignOut 
-    : productionRedirectSignOut, }
-}
-
-Amplify.configure(updatedAwsConfig);
-
-AppRegistry.registerComponent(appName, () => App);
+ import '@azure/core-asynciterator-polyfill';
+ import {AppRegistry} from 'react-native';
+ import App from './App';
+ import {name as appName} from './app.json';
+ import { Amplify } from 'aws-amplify';
+ import awsconfig from './src/aws-exports';
+ 
+ Amplify.configure(awsconfig);
+ 
+ AppRegistry.registerComponent(appName, () => App);
