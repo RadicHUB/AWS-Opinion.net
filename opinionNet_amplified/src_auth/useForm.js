cc17bb611544f1c216370e29afd9aca44763abe4
JGargana1 <54683595+JGargana1@users.noreply.github.com>
2022-12-08 15:31:46 -0500

    Update useForm.js

--- opinionNet_amplified/src_auth/useForm.js
+++ opinionNet_amplified/src_auth/useForm.js
@@ -3,13 +3,13 @@ import {useState} from 'react';
 export default function useForm(callback, initialState = {}, validate) {
   const [values, setValues] = useState(initialState);
   const [errors, setErrors] = useState({});
-
+//checks to see if a value was changed, then sets a new value
   function onChange({name, value}) {
     let newValue = value;
     if (name !== 'password') newValue = value.toLowerCase();
     setValues({...values, [name]: newValue});
   }
-
+//checks to see if some sort of submission has been made, then changes state
   function onSubmit() {
     const errors = validate(values);
     const isEmpty = !Object.values(errors).some(
